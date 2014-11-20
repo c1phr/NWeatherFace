13c595e7bb7cdc4ce7da6ac2faf67f1f348a28c4
Ryan Batchelder <rb688@nau.edu>
2014-11-20 14:37:58 -0700

    Adding layout for weather layer, pushing to grab font

--- src/main.c
+++ src/main.c
@@ -6,79 +6,89 @@ static TextLayer *s_weather_layer;
 
 static void update_time()
 {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
-
-  // Create a long-lived buffer
-  static char buffer[] = "00:00";
-
-  // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-    // Use 24 hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  } else {
-    // Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-  }
-
-  // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
+    // Get a tm structure
+    time_t temp = time(NULL);
+    struct tm *tick_time = localtime(&temp);
+    
+    // Create a long-lived buffer
+    static char buffer[] = "00:00";
+    
+    // Write the current hours and minutes into the buffer
+    if(clock_is_24h_style() == true)
+    {
+        // Use 24 hour format
+        strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    }
+    else
+    {
+        // Use 12 hour format
+        strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    }
+    
+    // Display this time on the TextLayer
+    text_layer_set_text(s_time_layer, buffer);
 }
 
 static void main_window_load(Window *window) {
-  // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 114, 144, 50));
-  text_layer_set_background_color(s_time_layer, GColorBlack);
-  text_layer_set_text_color(s_time_layer, GColorWhite);
-  text_layer_set_text(s_time_layer, "00:00");
+    // Create time TextLayer
+    s_time_layer = text_layer_create(GRect(0, 114, 144, 50));
+    text_layer_set_background_color(s_time_layer, GColorBlack);
+    text_layer_set_text_color(s_time_layer, GColorWhite);
+    text_layer_set_text(s_time_layer, "00:00");
+    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
+    text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+    
+    // Create weather TextLayer
+    s_weather_layer = text_layer_create(GRect(0, 10, 144, 25));
+    text_layer_set_background_color(s_weather_layer, GColorBlack);
+    text_layer_set_text_color(s_weather_layer, GColorWhite);
+    text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+    text_layer_set_text(s_weather_layer, "Loading...");
+//    text_layer_set_font(s_weather_layer, fonts_get_system_font());
 
-  // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
-  window_set_background_color(s_main_window, GColorBlack);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+    
+    window_set_background_color(s_main_window, GColorBlack);
+    
+    // Add it as a child layer to the Window's root layer
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
-  text_layer_destroy(s_time_layer);
+    text_layer_destroy(s_time_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
-  update_time();
+    update_time();
 }
 
 static void init()
 {
-  // Create main Window element and assign to pointer
-  s_main_window = window_create();
-
-  // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
-
-  // Show the Window on the watch, with animated=true
-  window_stack_push(s_main_window, true);
-  
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  update_time();
+    // Create main Window element and assign to pointer
+    s_main_window = window_create();
+    
+    // Set handlers to manage the elements inside the Window
+    window_set_window_handlers(s_main_window, (WindowHandlers) {
+        .load = main_window_load,
+        .unload = main_window_unload
+    });
+    
+    // Show the Window on the watch, with animated=true
+    window_stack_push(s_main_window, true);
+    
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+    update_time();
 }
 
 static void deinit()
 {
-  window_destroy(s_main_window);
+    window_destroy(s_main_window);
 }
 
 int main(void)
 {
-  init();
-  app_event_loop();
-  deinit();
-  return 0;
+    init();
+    app_event_loop();
+    deinit();
+    return 0;
 }
