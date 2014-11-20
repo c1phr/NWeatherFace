3a37cbe586084ebfd794f928afadb08f12c2be67
Ryan Batchelder <rb688@nau.edu>
2014-11-20 15:13:49 -0700

    Setup field for weather and getting ready to actually pull data

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <pebble.h>
 #include <pebble.h>
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
 static Window *s_main_window;
+static GFont s_time_font;
+static GFont s_text_font;
 static TextLayer *s_time_layer;
 static TextLayer *s_weather_layer;
 
@@ -31,35 +35,58 @@ static void update_time()
 
 static void main_window_load(Window *window) {
     // Create time TextLayer
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_REG_34));
+    s_text_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_REG_20));
+    
     s_time_layer = text_layer_create(GRect(0, 114, 144, 50));
     text_layer_set_background_color(s_time_layer, GColorBlack);
     text_layer_set_text_color(s_time_layer, GColorWhite);
     text_layer_set_text(s_time_layer, "00:00");
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
+    text_layer_set_font(s_time_layer, s_time_font);
     text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
     
     // Create weather TextLayer
-    s_weather_layer = text_layer_create(GRect(0, 10, 144, 25));
+    s_weather_layer = text_layer_create(GRect(0, 10, 144, 40));
     text_layer_set_background_color(s_weather_layer, GColorBlack);
     text_layer_set_text_color(s_weather_layer, GColorWhite);
     text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
     text_layer_set_text(s_weather_layer, "Loading...");
-//    text_layer_set_font(s_weather_layer, fonts_get_system_font());
+    text_layer_set_font(s_weather_layer, s_text_font);
 
     
     window_set_background_color(s_main_window, GColorBlack);
     
-    // Add it as a child layer to the Window's root layer
+    // Add children to the main window layer
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
 }
 
 static void main_window_unload(Window *window) {
     text_layer_destroy(s_time_layer);
+    fonts_unload_custom_font(s_time_font);
+    fonts_unload_custom_font(s_text_font);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
     update_time();
+}
+
+static void inbox_recieved_callback(DictionaryIterator *iterator, void *context)
+{
+    
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 static void init()
@@ -75,6 +102,12 @@ static void init()
     
     // Show the Window on the watch, with animated=true
     window_stack_push(s_main_window, true);
+    
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
     
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
     update_time();
