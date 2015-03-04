501f3651a68ea22cb2816405b5088ef5edc06834
Ryan Batchelder <ryanbatch@gmail.com>
2015-03-04 10:44:36 -0700

    Update weather data more often

--- src/main.c
+++ src/main.c
@@ -97,7 +97,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
     update_time();
     
-    if (tick_time->tm_min % 60 == 0)
+    if (tick_time->tm_min % 30 == 0)
     {
         DictionaryIterator *iter;
         app_message_outbox_begin(&iter);
